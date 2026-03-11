1247a34a7dbfe7f9665cfd2bdea2efe9853bd9e5
yefryserna4-prog <yefryserna4@gmail.com>
2026-03-11 18:31:11 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // En Tailwind v4, la mayoría de la configuración se hace en CSS
+  // Este archivo es opcional y solo se usa para opciones avanzadas
+}
